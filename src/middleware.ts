2382f814322951f77880c0f4af64f04ff3aa21f5
Zakariyya Ahmed <zakariyya_ahmed@outlook.com>
2025-09-20 19:02:06 +0100

    feat: create middleware, create updateSession function

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware.sb";
+
+const middleware = async (req: NextRequest) => {
+  return updateSession(req);
+};
+export const config = {
+  matcher: ["/private/:path*"],
+  /*
+   * Match all request paths except for the ones starting with:
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   * */
+  // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+};
+
+export default middleware;
