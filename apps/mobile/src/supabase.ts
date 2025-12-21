49e836c65410ed9e191d90f7b9cdd5df11f49552
Zakariyya Ahmed <zakariyya_ahmed@outlook.com>
2025-12-21 17:13:30 +0000

    feat: update expo env var

--- apps/mobile/src/supabase.ts
+++ apps/mobile/src/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
 );
