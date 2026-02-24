57851b4e59534ab250daccac4b9945ecfef8c8b8
zak ahmed personal <zakariyya_ahmed@outlook.com>
2026-02-24 16:04:39 +0000

    chore: setup and run lint

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,6 +1,6 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/use-theme-color';
+import { useThemeColor } from "@/hooks/use-theme-color";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
@@ -8,7 +8,6 @@ export type ThemedViewProps = ViewProps & {
 };
 
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, "background");
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const prettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: [".expo/**", "dist/**", "web-build/**", "expo-env.d.ts"],
   },
+  prettierRecommended,
 ]);
