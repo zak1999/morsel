966782ed2c5aee4d3a1bafb2fc2817947bf19548
zak ahmed personal <zakariyya_ahmed@outlook.com>
2026-03-04 18:04:53 +0000

    chore: install tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
